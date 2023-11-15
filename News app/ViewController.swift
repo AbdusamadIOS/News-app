eb1688052e0816691a0bc1c35b3f2d9c389fb210
abdusamad <abdusamad2609@gmail.com>
2023-11-15 14:21:14 +0500

    Initial Commit

---
+++ News app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News app
+//
+//  Created by Abdusamad Mamasoliyev on 15/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
